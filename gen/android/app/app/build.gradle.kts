a7751a869ba38731943b73bb1d433c4b6dd8656e
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2022-12-29 20:53:49 -0500

    🔖 Update version

--- gen/android/app/app/build.gradle.kts
+++ gen/android/app/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.kaetram.app"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.2.7"
     }
     sourceSets.getByName("main") {
         // Vulkan validation layers
