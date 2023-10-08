55a2d4dee1f38ce827877014bd3af8a12becffba
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2023-10-08 18:43:28 -0400

    fix: patch code for older androids

--- gen/android/app/src/main/java/com/kaetram/app/MainActivity.kt
+++ gen/android/app/src/main/java/com/kaetram/app/MainActivity.kt
@@ -1,3 +1,21 @@
 package com.kaetram.app
 
-class MainActivity : TauriActivity()
+import android.view.KeyEvent
+
+class MainActivity : TauriActivity() {
+  private lateinit var mWebView: RustWebView
+
+  private fun setWebView(webView: RustWebView) {
+    mWebView = webView
+  }
+
+  override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+    if (keyCode == KeyEvent.KEYCODE_BACK && mWebView.canGoBack()) {
+      mWebView.goBack()
+
+      return true
+    }
+
+    return super.onKeyDown(keyCode, event)
+  }
+}
