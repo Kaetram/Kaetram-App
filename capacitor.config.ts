dee4f12fd693cedab165b5b92db7efe755eb2f5e
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2023-10-12 03:48:23 -0400

    refactor: test capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+export default {
+    appId: 'com.kaetram.app',
+    appName: 'Kaetram',
+    webDir: '../client/dist/',
+    backgroundColor: '#000000',
+    server: { androidScheme: 'https' }
+} satisfies CapacitorConfig;
