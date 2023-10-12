05e7bff03170cd166e91dd3df38695438f816a80
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2023-10-12 19:11:03 -0400

    feat: fullscreen activity

--- android/app/src/main/java/com/kaetram/app/MainActivity.java
+++ android/app/src/main/java/com/kaetram/app/MainActivity.java
@@ -1,5 +1,43 @@
 package com.kaetram.app;
 
-import com.getcapacitor.BridgeActivity;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends BridgeActivity {}
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+
+import java.util.ArrayList;
+
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        hideSystemUI();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) hideSystemUI();
+    }
+
+    private void hideSystemUI() {
+        final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        View decorView = getWindow().getDecorView();
+
+        decorView.setSystemUiVisibility(flags);
+        decorView.setOnSystemUiVisibilityChangeListener((int visibility) -> {
+            if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0)
+                decorView.setSystemUiVisibility(flags);
+        });
+    }
+}
